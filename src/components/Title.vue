24730686bf9c34cce113d3fc53dc3bf99d25ea19
Fabiopanetta02 <panetta.fabio20@gmail.com>
2022-07-21 14:36:30 +0200

    Added Section with Title

---
+++ src/components/Title.vue
@@ -0,0 +1,19 @@
+
+<template>
+    <h1>La mia prima app con Vue CLI</h1>
+</template>
+
+
+<script>
+export default {
+/* eslint-disable */
+  name: "Title",
+}
+</script>
+
+
+<style>
+h1 {
+    color: red;
+}
+</style>
